7be4ade0b63f45c4d822def998289d10c71c1ece
Jacob Moore <jacobmoor2@gmail.com>
2025-03-31 13:22:06 -0600

    python file for debugging and using NaVid for our CARL framework

---
+++ CARL.py
@@ -0,0 +1,35 @@
+import argparse
+from navid_agent import NaVid_Agent
+import cv2 as cv
+
+
+# Maybe we make a class here for running / interfacing with CARL?
+
+
+
+# For debugging
+if __name__=='__main__':
+    model_path = "model_zoo/navid-7b-full-224-video-fps-1-grid-2-r2r-rxr-training-split"
+    result_path = "output"
+
+    agent = NaVid_Agent(model_path, result_path, require_map=False)
+
+    # Get live images
+    cap = cv.VideoCapture(0)
+
+    env_id = 0
+    obs = {}
+    while True:
+        # Get observations
+        ret, frame = cap.read()
+        obs["rgb"] = frame
+        obs["instruction"]["text"] = "Go across the room to the black table and stop by the chair."
+
+        # Get info
+        # Since we aren't using the sim, we can ignore this!
+        info = None
+
+        # Get agent action
+        action = agent.act(obs, info, env_id)
+
+        env_id += 1
