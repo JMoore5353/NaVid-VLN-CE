dc251cf3b495844b10641782d7da53c517373597
JMoore5353 <jacobmoor2@gmail.com>
2025-04-03 22:01:04 -0600

    added docker runtime commands to access gpus and cameras

--- CARL.py
+++ CARL.py
@@ -1,5 +1,6 @@
 import argparse
 import cv2 as cv
+import time
 
 from navid_agent import NaVid_Agent
 
@@ -16,17 +17,24 @@ if __name__=='__main__':
     agent = NaVid_Agent(model_path, result_path, require_map=False)
 
     # Get live images
+    print("Done initializing agent")
     cap = cv.VideoCapture(0)
+    print("Initialized camera")
 
     env_id = 0
     obs = {}
     while True:
+        start_time = time.time()
         # Get observations
         ret, frame = cap.read()
+        cam_time = time.time() - start_time
         if not ret:
+            print("Error reading from camera!")
             break
 
+        start_time = time.time()
         obs["rgb"] = frame
+        obs["instruction"] = {}
         obs["instruction"]["text"] = "Go across the room to the black table and stop by the chair."
 
         # Get info
@@ -35,5 +43,9 @@ if __name__=='__main__':
 
         # Get agent action
         action = agent.act(obs, info, env_id)
+        print("Getting action: ", action["action"], "  Inference time:", round(time.time() - start_time, 6), "Cam time:", round(cam_time, 6), "Env ID:", env_id)
 
         env_id += 1
+
+        if env_id % 100 == 0:
+            agent.reset()
